80245a24a5b0f82a7cd2bf616012f19706efb1bc
zhousj <81700917@qq.com>
2017-01-22 12:15:55 +0800

    111

---
+++ src/main/java/com/zsj/api/UserApplication.java
@@ -0,0 +1,12 @@
+package com.zsj.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zsj on 2017/1/22.
+ */
+@RestController
+@RequestMapping("zsj/user")
+public class UserApplication {
+}
