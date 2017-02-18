18205b73b063d01aca71f7af0f932d952a651f74
81700917@qq.com <81700917@qq.com>
2017-02-18 21:04:07 +0800

    111

---
+++ src/main/java/com/zsj/dao/ObjDao.java
@@ -0,0 +1,9 @@
+package com.zsj.dao;
+
+/**
+ * Created by zsj on 2017/2/18.
+ */
+public interface ObjDao {
+
+
+}
---
+++ src/main/java/com/zsj/model/ResultMessage.java
@@ -0,0 +1,8 @@
+package com.zsj.model;
+
+/**
+ * Created by zsj on 2017/2/18.
+ */
+public class ResultMessage {
+
+}
